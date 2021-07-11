b1e5046352566eae6f0cd3c31450d89e3a1d1615
Aaron Dewes <aaron.dewes@web.de>
2021-07-11 11:43:06 +0100

    Export validator directly

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./safeHandler";
 export * from "./camelize";
 export * from "./errors";
 export * from "./validator";
+export * as validator from "./validator";
 export { default as convert } from "./convert";
