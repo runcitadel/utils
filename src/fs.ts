477a764c39da0c4553e59e503df77e2027af14fe
Aaron Dewes <aaron.dewes@web.de>
2021-07-12 16:40:35 +0100

    Add new fs methods

---
+++ src/fs.ts
@@ -0,0 +1,59 @@
+import * as fs from "node:fs/promises";
+import * as crypto from "node:crypto";
+const uint32Bytes = 4;
+
+/**
+ * Reads a file with utf-8 encoding into a string
+ *
+ * @param filePath The path of the file to read
+ * @returns The content of the file
+ */
+export async function readUtf8File(filePath: string): Promise<string> {
+    return await fs.readFile(filePath, "utf8");
+}
+
+/**
+ * Reads a JSON file into an object
+ *
+ * @param filePath The path of the file to read
+ * @returns The parsed content of the file
+ */
+export async function readJsonFile(filePath: string): Promise<unknown> {
+    return await readUtf8File(filePath).then(JSON.parse);
+}
+
+/**
+ * Safely writes into a file (by writing to a temp file, then renaming)
+ * 
+ * @param filePath The path of the file to write
+ * @param str The data to write (encoded as a string)
+ */
+ export async function safeWriteFile(
+    filePath: string,
+    str: string,
+): Promise<void> {
+    const tempFileName = `${filePath}.${crypto
+        .randomBytes(uint32Bytes)
+        .readUInt32LE(0)}`;
+
+    await fs.writeFile(tempFileName, str);
+    try {
+        await fs.rename(tempFileName, filePath);
+    } catch (err) {
+        await fs.unlink(tempFileName);
+        throw err;
+    }
+}
+
+/**
+ * Encodes data to JSON and writes it into a file
+ * 
+ * @param filePath The path of the file to write
+ * @param obj The data to be written into the file (as an object)
+ */
+export async function writeJsonFile(
+    filePath: string,
+    obj: unknown
+): Promise<void> {
+    safeWriteFile(filePath, JSON.stringify(obj));
+}
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./safeHandler.js";
 export * from "./camelize.js";
 export * from "./errors.js";
 export * as validator from "./validator.js";
+export * as fs_utils from "./fs.js";
 export { default as convert } from "./convert.js";
