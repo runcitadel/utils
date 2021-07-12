5e44e768d8947d5155fef7c36ba2f85a57154c63
Aaron Dewes <aaron.dewes@web.de>
2021-07-12 16:50:23 +0100

    Small fixes

--- src/fs.ts
+++ src/fs.ts
@@ -24,13 +24,13 @@ export async function readJsonFile(filePath: string): Promise<unknown> {
 
 /**
  * Safely writes into a file (by writing to a temp file, then renaming)
- * 
+ *
  * @param filePath The path of the file to write
  * @param str The data to write (encoded as a string)
  */
- export async function safeWriteFile(
+export async function safeWriteFile(
     filePath: string,
-    str: string,
+    str: string
 ): Promise<void> {
     const tempFileName = `${filePath}.${crypto
         .randomBytes(uint32Bytes)
@@ -47,7 +47,7 @@ export async function readJsonFile(filePath: string): Promise<unknown> {
 
 /**
  * Encodes data to JSON and writes it into a file
- * 
+ *
  * @param filePath The path of the file to write
  * @param obj The data to be written into the file (as an object)
  */
