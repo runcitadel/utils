35bf837a61614c29119f99dcfa35a7263a1883b3
Aaron Dewes <aaron.dewes@web.de>
2021-10-31 08:51:41 +0000

    Remove unused camelize function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { default as corsOptions } from "./cors.js";
 export { default as errorHandler } from "./errorHandler.js";
-export * from "./camelize.js";
 export * from "./errors.js";
 export * as typeHelper from "./typeHelper.js";
 export * from "./utils.js";
